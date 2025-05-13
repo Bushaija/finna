f30b5ff367140fa8d8ad1ccb82f0f83173b7cc35
bushaija <Bushaijarobert@gmail.com>
2025-05-13 10:03:57 +0200

    standalone config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+};
 
-eslint: {
+module.exports = {
+  output: "standalone",
+  eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true, 
@@ -15,6 +18,7 @@ eslint: {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
-};
+}
+
 
 export default nextConfig;
